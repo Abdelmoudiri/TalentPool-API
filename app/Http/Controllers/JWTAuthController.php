e27f60973f8f6debc673eb641be8401806e1532a
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-03-19 19:38:03 +0000

    implement login, registration, and logout methods in JWTAuthController; replace LoginhRequest with LoginRequest

--- app/Http/Controllers/JWTAuthController.php
+++ app/Http/Controllers/JWTAuthController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class JWTAuthController extends Controller
 {
-    //
+    public function login(LoginRequest $request)
+    {
+        $request->validated();
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Credentials don\'t match'
+            ], 401);
+        }
+    
+        $user = User::where('email', $request->email)->first();
+    
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('token for ' . $user->email)->plainTextToken
+        ], 200);
+    }
+    
+
+    public function register(RegisterRequest $request){
+        $request->validated($request->all());
+
+        $user = User::create([
+            'fname' => $request->fname,
+            'lname' => $request->lname,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role
+        ]);
+
+        return response()->json([
+            'user' => $user,
+            'token' => $user->createToken('token for '.$user->email)->plainTextToken
+        ], 201);
+    }
+
+    public function logout(){
+        Auth::user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'You have been logged out'
+        ], 200);
+    }
 }
