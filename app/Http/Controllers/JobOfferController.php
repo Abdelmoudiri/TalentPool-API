d02e27d302c94549d438c048d7859b57d14671cf
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2025-03-19 15:16:35 +0000

    add JobOffer model, controller, requests, policy, factory, migration, and seeder

---
+++ app/Http/Controllers/JobOfferController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JobOffer;
+use App\Http\Requests\StoreJobOfferRequest;
+use App\Http\Requests\UpdateJobOfferRequest;
+
+class JobOfferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreJobOfferRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JobOffer $jobOffer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateJobOfferRequest $request, JobOffer $jobOffer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JobOffer $jobOffer)
+    {
+        //
+    }
+}
